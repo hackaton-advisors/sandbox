650f2a5b9213e8218fc226fff48b8aa2d405a3b0
villemm <ville.maanpaa@gmail.com>
2015-03-10 20:25:34 +0000

    port changed

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var db = require('./config/db');
 
 // set our port
 //var port = process.env.PORT || 80;
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080  , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080  , ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
 // connect to our mongoDB database 
 // (uncomment after you enter in your own credentials in config/db.js)
@@ -40,7 +40,18 @@ require('./app/routes')(app); // configure our routes
 
 // start app ===============================================
 // startup our app at http://localhost:8080
-app.listen(port);               
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+
+
+app.listen(server_port, server_ip_address, function(){
+
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+
+});
+//app.listen(port);               
 
 // shoutout to the user                     
 console.log('Magic happens on port ' + port);
