bddb8faeed2d1dd74f5c8ec8291f2d38affcec58
villemm <ville.maanpaa@gmail.com>
2015-03-10 23:38:44 +0000

    links changed

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -9,12 +9,20 @@
             controller: 'MainController'
         })
 
-        // nerds page that will use the NerdController
         .when('/funddata', {
             templateUrl: 'views/funddata.html',
             controller: 'FundDataController'
+        })
+        
+        .when('/maps', {
+            templateUrl: 'views/maps.html',
+            controller: 'FundDataController'
+        })
+        
+        .when('/charts', {
+            templateUrl: 'views/charts.html',
+            controller: 'FundDataController'
         });
-
     $locationProvider.html5Mode(true);
 
 }]);
